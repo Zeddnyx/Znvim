3f300f7847f961dbc93fdae096747bec0eed56fb
Zeddnyx <96564938+Zeddnyx@users.noreply.github.com>
2022-10-08 10:26:32 +0700

    Delete nvim directory

--- nvim/after/plugin/mason.lua
+++
@@ -1,8 +0,0 @@
-local ok, mason = pcall(require, "mason")
-if not ok then return end
-
-mason.setup {}
-require("mason-lspconfig").setup {
-	ensured_installed = { "tsserver", "intelephense", "eslint" },
-	automatic_installation = true
-}
