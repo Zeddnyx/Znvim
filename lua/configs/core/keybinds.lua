9696119adcbd5d4ad4765522990ebe99286f91c8
Zeddnyx <zeddnyx08@gmail.com>
2023-04-11 16:17:10 +0700

    update keybind

--- lua/configs/core/keybinds.lua
+++ lua/configs/core/keybinds.lua
@@ -24,21 +24,23 @@ function nshort(shortcut, command)
 	short("n", shortcut, command)
 end
 
+nmap("<S-j>", "")
+nmap("<S-k>", "")
 
 -- buffer
-nmap("<S-h>", "<CMD>bprevious<CR>", { desc = "Buffer: focus to previous buffer" })
-nmap("<S-l>", "<CMD>bnext<CR>", { desc = "Buffer: focus to next buffer" })
+nmap("<S-h>", "<CMD>bprevious<CR>", { desc = "Buffer: previous buffer" })
+nmap("<S-l>", "<CMD>bnext<CR>", { desc = "Buffer: next buffer" })
 nmap("<S-c>", "<CMD>bd<CR>", { desc = "Buffer: delete buffer" })
 nmap("<S-q>q", "<CMD>%bd<CR>", { silent = true, desc = "Buffer: delete all buffer" })
 nmap("<S-q>a", "<CMD>%bd!<CR>", { silent = true, desc = "Buffer: force delete all buffer" })
 nmap("<S-q>o", "<CMD>%bd|e#<CR>", { silent = true, desc = "Buffer: delete all buffer except this one" })
 
--- split screen
+-- switch screen
 nmap("<C-h>", "<C-w>h<cr>") -- Switch right
 nmap("<C-j>", "<C-w>j<cr>") -- Switch down
 nmap("<C-k>", "<C-w>k<cr>") -- Switch up
 nmap("<C-l>", "<C-w>l<cr>") -- Switch left
 
--- costume keybind
-nmap("<leader>g", "gg<S-v>G") -- select all
+-- costume shortcut
+nmap("<leader>a", "gg<S-v>G") -- select all
 nshort("<leader>s", [[:%s/]]) -- find and replace all matched word
