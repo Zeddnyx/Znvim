74552da73aa534b6a3a211ef9557459496e89ef0
Zeddnyx <zeddnyx08@gmail.com>
2023-05-12 19:09:04 +0700

    change telescope look like

--- lua/configs/ui/telescope.lua
+++ lua/configs/ui/telescope.lua
@@ -1,87 +1,185 @@
-local M = {}
+local telescope = require("telescope")
+local config = {}
 
-function M.config()
-	local telescope = require("telescope")
-	local previewers = require("telescope.previewers")
-	local sorters = require("telescope.sorters")
+config.defaults = {
+  layout_strategy = "flex",
+  layout_config = {
+    horizontal = { preview_width = 80 },
+  },
+  prompt_prefix = "   ",
+  selection_caret = "  ", -- SEE: https://github.com/nvim-telescope/telescope.nvim/issues/1841
+  dynamic_preview_title = true,
+  results_title = false,
+  mappings = {
+    n = {
+      ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
+    },
+    i = {
+      ["<C-u>"] = false,
+      ["<C-d>"] = false,
+      ["<C-f>"] = require("telescope.actions").preview_scrolling_down,
+      ["<C-b>"] = require("telescope.actions").preview_scrolling_up,
+      ["<C-s>"] = require("telescope.actions").select_horizontal,
+      ["<M-p>"] = require("telescope.actions.layout").toggle_preview,
+      ["<C-[>"] = require("telescope.actions").close,
+      ["<C-j>"] = require("telescope.actions").cycle_history_next,
+      ["<C-k>"] = require("telescope.actions").cycle_history_prev,
+    },
+  },
+  -- SEE: https://developer.roblox.com/en-us/articles/string-patterns-reference
+  file_ignore_patterns = {
+    "%.class$",
+    "%.dmg$",
+    "%.gif$",
+    "%.gz$",
+    "%.iso$",
+    "%.jar$",
+    "%.jpg$",
+    "%.jpeg$",
+    "%.JPEG$",
+    "%.mkv$",
+    "%.mp4$",
+    "%.o$",
+    "%.otf$",
+    "%.out$",
+    "%.pdf$",
+    "%.png$",
+    "%.PNG$",
+    "%.pyc$",
+    "%.pyi$",
+    "%.tar",
+    "%.torrent$",
+    "%.ttf$",
+    "%.webm$",
+    "%.webp$",
+    "%.zip$",
+    "^.dart_tool/",
+    "^.git/",
+    "^.github/",
+    "^.gradle/",
+    "^.idea/",
+    "^.settings/",
+    "^.vscode/",
+    "^.env/",
+    "^__pycache__/",
+    "^bin/",
+    "^build/",
+    "^env/",
+    "^gradle/",
+    "^node_modules/",
+    "^obj/",
+    "^target/",
+    "^vendor/",
+    "^zig%-cache/",
+    "^zig%-out/",
+  },
+}
 
-	local options = {
-		defaults = {
-			vimgrep_arguments = {
-				"rg",
-				"--color=never",
-				"--no-heading",
-				"--with-filename",
-				"--line-number",
-				"--column",
-				"--smart-case",
-			},
-			results_title = "",
-			entry_prefix = "   ",
-			initial_mode = "insert",
-			selection_strategy = "reset",
-			file_ignore_patterns = {
-				"^node_modules/",
-				".git",
-				"%.svg$",
-				"%.png$",
-        ".png",
-				".jpeg",
-				".jpg",
-				"assets/**",
-				"*/assets",
-				"assets",
-				".mp3",
-				".mp4",
-				"public/",
-				"public/**",
-				"*/public",
-			},
-			file_sorter = sorters.get_fzy_sorter,
-			generic_sorter = sorters.get_generic_fzy_sorter,
-			path_display = { "absolute" },
-			winblend = 5,
-			color_devicons = true,
-			use_less = false,
-			file_previewer = previewers.vim_buffer_cat.new,
-			grep_previewer = previewers.vim_buffer_vimgrep.new,
-			qflist_previewer = previewers.vim_buffer_qflist.new,
-			-- custom theme
-			sorting_strategy = "descending",
-		},
-		pickers = {
-			diagnostics = {
-				previewer = false,
-				line_width = 0.7,
-				no_unlisted = true,
-				path_display = {
-					"hidden",
-				},
-			},
-			find_files = {
-				previewer = true,
-			},
-			oldfiles = {
-				previewer = false,
-				file_ignore_patterns = {
-					"/usr/share/nvim/runtime/*",
-					vim.fn.stdpath("data"),
-					vim.fn.stdpath("cache"),
-					vim.fn.stdpath("log"),
-				},
-			},
-			git_files = {
-				hidden = true,
-				show_untracked = true,
-			},
-			live_grep = {
-				--@usage don't include the filename in the search results
-				only_sort_text = true,
-			},
-			grep_string = {
-				only_sort_text = true,
-			},
-		},
-	}
-end
-return M.config()
+local pickers = {}
+
+pickers.find_files = {
+  layout_config = { height = 0.4 },
+  theme = "ivy",
+  previewer = false,
+}
+
+pickers.buffers = {
+  prompt_prefix = "   ",
+  path_display = { "smart" },
+  theme = "dropdown",
+  previewer = false,
+  mappings = {
+    i = {
+      ["<C-d>"] = require("telescope.actions").delete_buffer,
+    },
+  },
+}
+
+pickers.help_tags = {
+  prompt_prefix = " 龎 ",
+  theme = "dropdown",
+  previewer = false,
+}
+
+pickers.live_grep = {
+  layout_config = {
+    anchor = "N",
+    height = 0.35,
+    mirror = true,
+    width = 0.55,
+  },
+  prompt_prefix = "   ",
+  theme = "dropdown",
+}
+
+pickers.filetypes = {
+  layout_config = {
+    width = 0.25,
+    height = 0.5,
+  },
+  prompt_prefix = "   ",
+}
+
+pickers.git_files = { prompt_prefix = "   " }
+pickers.git_commits = {
+  layout_config = pickers.live_grep.layout_config,
+  prompt_prefix = "   ",
+  theme = "dropdown",
+}
+pickers.git_bcommits = pickers.git_commits
+pickers.git_branches = {
+  prompt_prefix = "   ",
+  theme = "dropdown",
+  previewer = false,
+}
+pickers.git_status = {
+  layout_config = { horizontal = { preview_width = 0.55 } },
+  prompt_prefix = "   ",
+}
+
+pickers.lsp_document_symbols = {
+  layout_config = {
+    anchor = "N",
+    height = 0.35,
+    mirror = true,
+    width = 0.50,
+  },
+  previewer = false,
+  prompt_prefix = "   ",
+  theme = "dropdown",
+}
+pickers.lsp_workspace_symbols = {
+  layout_config = { height = 0.4 },
+  prompt_prefix = "   ",
+  theme = "ivy",
+}
+pickers.lsp_references = { theme = "dropdown" }
+pickers.lsp_implementations = pickers.lsp_references
+pickers.lsp_definitions = pickers.lsp_references
+
+pickers.diagnostics = {
+  layout_config = { height = 0.4 },
+  prompt_prefix = "   ",
+  theme = "ivy",
+  previewer = false,
+}
+
+config.pickers = pickers
+-- config.extensions = {
+--   file_browser = {
+--     prompt_prefix = "   ",
+--     hide_parent_dir = true,
+--     mappings = {
+--       i = {
+--         ["<C-e>"] = telescope.extensions.file_browser.actions.goto_parent_dir,
+--         ["<M-f>"] = telescope.extensions.file_browser.actions.toggle_browser,
+--       },
+--     },
+--   },
+--   fzf = {},
+-- }
+
+telescope.setup(config)
+-- for extension, _ in pairs(config.extensions) do
+--   telescope.load_extension(extension)
+-- end
