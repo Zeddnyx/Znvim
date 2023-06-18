cb8bba10169e4d901b673cfafa6d1deb4778cb32
Zeddnyx <96564938+Zeddnyx@users.noreply.github.com>
2023-06-19 06:12:11 +0700

    update filemanager view to float

--- lua/zedd/ui/filemanager.lua
+++ lua/zedd/ui/filemanager.lua
@@ -9,13 +9,34 @@ local options = {
 	hijack_unnamed_buffer_when_opening = false,
 	sync_root_with_cwd = true,
 	view = {
-		adaptive_size = true,
-		side = "left",
-		width = 30,
-		preserve_window_proportions = true,
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * 100
+        local window_h = screen_h * 100
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * 5)
+    end,
 	},
 	git = {
-		enable = false,
+		enable = true,
 		ignore = true,
 	},
 	filesystem_watchers = {
@@ -49,7 +70,7 @@ local options = {
 				file = true,
 				folder = true,
 				folder_arrow = false,
-				git = false,
+				git = true,
 			},
 
 			glyphs = {
