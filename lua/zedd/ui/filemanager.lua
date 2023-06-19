f6ae69c4af3e2f412210b4779b36224c6e0a1a39
Zedd <zeddnyx08@gmail.com>
2023-06-19 12:54:32 +0700

    change width filemanager float

--- lua/zedd/ui/filemanager.lua
+++ lua/zedd/ui/filemanager.lua
@@ -9,31 +9,27 @@ local options = {
 	hijack_unnamed_buffer_when_opening = false,
 	sync_root_with_cwd = true,
 	view = {
-    float = {
-      enable = true,
-      open_win_config = function()
-        local screen_w = vim.opt.columns:get()
-        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-        local window_w = screen_w * 100
-        local window_h = screen_h * 100
-        local window_w_int = math.floor(window_w)
-        local window_h_int = math.floor(window_h)
-        local center_x = (screen_w - window_w) / 2
-        local center_y = ((vim.opt.lines:get() - window_h) / 2)
-                         - vim.opt.cmdheight:get()
-        return {
-          border = 'rounded',
-          relative = 'editor',
-          row = center_y,
-          col = center_x,
-          width = window_w_int,
-          height = window_h_int,
-        }
-        end,
-    },
-    width = function()
-      return math.floor(vim.opt.columns:get() * 5)
-    end,
+		float = {
+			enable = true,
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local w_h = 100
+				local center_x = (screen_w - w_h) / 2
+				local center_y = ((vim.opt.lines:get() - w_h) / 2) - vim.opt.cmdheight:get()
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = w_h,
+					height = w_h,
+				}
+			end,
+		},
+		width = function()
+			return math.floor(vim.opt.columns:get() * 5)
+		end,
 	},
 	git = {
 		enable = true,
@@ -59,7 +55,7 @@ local options = {
 		root_folder_label = true,
 		highlight_git = false,
 		highlight_opened_files = "none",
-    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "LICENSE" },
+		special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", "LICENSE" },
 
 		indent_markers = {
 			enable = false,
