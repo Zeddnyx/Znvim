ae7599cccf73bb5a3395ed19abcf9f86bd92368d
Zeddnyx <zeddnyx08@gmail.com>
2024-10-21 18:57:40 +0700

    chore: update lsp, filemanager ignore

--- lua/zedd/lsp/lspconfig.lua
+++ lua/zedd/lsp/lspconfig.lua
@@ -16,7 +16,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-lsp.ts_ls.setup({
+lsp.tsserver.setup({
 	on_attach = on_attach,
 	handlers = {
 		["workspace/applyEdit"] = function()
